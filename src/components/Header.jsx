cb2042c85d37589d4b0deec324f4b24c812883fe
miguelfq <miguelfavioqm@gmail.com>
2023-05-17 22:48:12 -0400

    Mejora diseño Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,23 @@ import { HiMenuAlt2, HiX} from "react-icons/hi";
 const Header = () => {
     const[showMenu, setShowMenu] = useState(false);
     return (
-    <header className='flex w-full items-center p-4 h-[10vh] justify-between xl:justify-start px-6'>
+    <header className='flex w-full items-center p-4 h-[10vh] justify-between xl:justify-start pl-6 pr-1'>
         <div className='w-1/6 text-center'>
             <a href='#' className='text-2xl font-bold relative bg-white'>POWER
-            <span className='text-5xl text-purple-600'>.</span><BsFillCircleFill className='absolute -left-3 xl:-bottom-3 bottom-5 text-purple-600 -z-10'/></a>
+            <span className='text-5xl text-purple-600'>.</span><BsFillCircleFill className='absolute -left-3 -bottom-3 text-purple-600 -z-10'/></a>
         </div>
-        <nav className={`fixed bg-white w-full h-full right-0 top-0 xl:static flex flex-col xl:flex-row text-center gap-10 justify-center ${showMenu ? "left-0" : "-left-full"} transition-all ease-in-out duration-500 z-20`}>
+        <nav className={`fixed bg-white w-full h-full right-0 top-0 lg:static flex flex-col lg:flex-row text-center gap-10 justify-center ${showMenu ? "left-0" : "-left-full"} transition-all ease-in-out duration-500 z-20`}>
             <span className=' flex flex-row-reverse'>
-            <button onClick={() => setShowMenu(!showMenu)} className=' inline-block text-3xl xl:invisible'><HiX/></button>
+            <button onClick={() => setShowMenu(!showMenu)} className=' inline-block text-3xl lg:invisible'><HiX/></button>
             </span>
-            <div className='flex flex-col w-full h-full gap-10 xl:flex-row justify-center xl:items-end text-2xl'>
+            <div className='flex flex-col w-full h-full gap-10 lg:flex-row justify-center lg:items-end text-2xl'>
             <a href="#" className=''>Home</a>
             <a href="#" className=''>About Us</a>
             <a href="#" className=''>Services</a>
             <a href="#" className=''>Products</a>
             </div>
         </nav>
-        <button onClick={() => setShowMenu(!showMenu)} className=' text-3xl p-2 xl:hidden'>
+        <button onClick={() => setShowMenu(!showMenu)} className=' text-3xl p-2 lg:hidden'>
             <HiMenuAlt2/>
         </button>
     </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,13 @@ import { HiOutlinePlay} from "react-icons/hi";
 const Hero = () => {
   return (
     <section className='h-[90vh] grid md:grid-cols-8 mt-2'>
-        <div className=' md:col-span-5 flex items-center justify-center p-16'>
+        <div className=' md:col-span-5 flex items-center justify-center px-3 lg:p-16'>
             <div className=' flex flex-col gap-10'>
-                <h1 className='text-7xl font-bold leading-[8rem] xl:leading-[8rem]'>We Design Impactful <br /> Digital <span className=' text-purple-600 p-4 border-8 border-purple-600 relative'>Products
-                <BsFillCircleFill className='text-4xl absolute left-[-7%] top-[-15%] text-white ring-8 ring-purple-600 rounded-full'/>
-                <BsFillCircleFill className='text-4xl absolute left-[-7%] top-[85%]  text-white ring-8 ring-purple-600 rounded-full'/>
-                <BsFillCircleFill className='text-4xl absolute left-[95%] top-[85%]  text-white ring-8 ring-purple-600 rounded-full'/>
-                <BsFillCircleFill className='text-4xl absolute left-[95%] top-[-15%]  text-white ring-8 ring-purple-600 rounded-full'/>
+                <h1 className='text-7xl font-bold leading-tight lg:leading-[8rem]'>We Design Impactful <br /> Digital <span className=' text-purple-600 lg:p-4 lg:border-8 lg:border-purple-600 relative'>Products
+                <BsFillCircleFill className='text-4xl absolute left-[-7%] top-[-15%] text-white ring-8 ring-purple-600 rounded-full hidden lg:inline-block'/>
+                <BsFillCircleFill className='text-4xl absolute left-[-7%] top-[85%]  text-white ring-8 ring-purple-600 rounded-full hidden lg:inline-block'/>
+                <BsFillCircleFill className='text-4xl absolute left-[95%] top-[85%]  text-white ring-8 ring-purple-600 rounded-full hidden lg:inline-block'/>
+                <BsFillCircleFill className='text-4xl absolute left-[95%] top-[-15%]  text-white ring-8 ring-purple-600 rounded-full hidden lg:inline-block'/>
                 </span></h1>
                 <p className=' text-gray-500 text-2xl leading-6 '>Help find solutions with intuitive and in accordance with client business goals. we provide high-quality services.</p>
                 <div className=' flex items-center gap-10 px-4'>
